69f85415f9dc756b7f85c211f8b62c6aac3ae97c
root <cesaria77550@gmail.com>
2021-02-04 17:40:54 +0000

    Add Kernel and Operating System call

--- include/system.h
+++ include/system.h
@@ -9,19 +9,31 @@
 
 class System {
  public:
+  System(); 
   Processor& Cpu();                   // TODO: See src/system.cpp
   std::vector<Process>& Processes();  // TODO: See src/system.cpp
   float MemoryUtilization();          // TODO: See src/system.cpp
   long UpTime();                      // TODO: See src/system.cpp
   int TotalProcesses();               // TODO: See src/system.cpp
   int RunningProcesses();             // TODO: See src/system.cpp
-  std::string Kernel();               // TODO: See src/system.cpp
-  std::string OperatingSystem();      // TODO: See src/system.cpp
-
+  
+  // Getters and setters
+  
+  // set and get kernel 
+  void SetKernel();
+  std::string Kernel() const;         // TODO: See src/system.cpp
+  
+  // OS 
+  void SetOps();
+  std::string OperatingSystem() const;      // TODO: See src/system.cpp
+  
+  
   // TODO: Define any necessary private members
  private:
   Processor cpu_ = {};
   std::vector<Process> processes_ = {};
+  std::string kernel_; 
+  std::string ops_;
 };
 
 #endif
